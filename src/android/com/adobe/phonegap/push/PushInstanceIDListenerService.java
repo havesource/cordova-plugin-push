ed4719291571abb107e79b22aeeb0c08c7f90745
Erisu <ellis.bryan@gmail.com>
2020-09-01 12:38:31 +0900

    chore(android): remove unused imports

--- src/android/com/adobe/phonegap/push/PushInstanceIDListenerService.java
+++ src/android/com/adobe/phonegap/push/PushInstanceIDListenerService.java
@@ -1,18 +1,11 @@
 package com.adobe.phonegap.push;
 
-import android.content.Intent;
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.android.gms.tasks.OnSuccessListener;
-
-import org.json.JSONException;
-
-import java.io.IOException;
 
 public class PushInstanceIDListenerService extends FirebaseMessagingService implements PushConstants {
   public static final String LOG_TAG = "Push_InsIdService";
